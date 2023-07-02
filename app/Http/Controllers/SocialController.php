30022368047792b3da38190408be2484f9deefbc
ahbatawy <ahbatawy@gmail.com>
2023-07-02 13:04:27 +0300

    first commit

---
+++ app/Http/Controllers/SocialController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialController extends Controller
+{
+    public function redirect($service)
+    {
+        return socialite::driver($service)->redirect();
+    }
+
+    public function callback($service)
+    {
+        return $user = socialite::with($service)->user();
+    }
+}
